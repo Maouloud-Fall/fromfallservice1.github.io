43380806d698c7bcf20982bb34f8718ef69bc6d3
Maouloud-Fall <140567869+Maouloud-Fall@users.noreply.github.com>
2023-07-27 00:10:46 +0000

    Add files via upload

---
+++ tp2/operation.php
@@ -0,0 +1,18 @@
+<?php
+$nombre1recu=$_GET['nombre1'];
+$nombre2recu=$_GET['nombre2'];
+$operateurrecu=$_GET['operateur'];
+$somme=;
+$soustraction;
+$multiplication;
+$division;
+$operateur;
+if(operateur='+')
+echo 'la somme est:',$somme<br>;
+if(operateur='-')
+echo 'la soustraction est:',$soustraction<br>;
+if(operateur='*')
+echo 'la multiplication est:',$multiplication<br>;
+if(operateur='/')
+echo 'la division est:',$division<br>;
+?>
